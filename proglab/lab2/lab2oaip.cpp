230d7ced50b00714ef32a2797d5f84846ebbc63e
shademytears <team218mighty@gmail.com>
2025-11-22 05:19:55 +0300

    Update lab2oaip.cpp

--- proglab/lab2/lab2oaip.cpp
+++ proglab/lab2/lab2oaip.cpp
@@ -15,7 +15,6 @@
 
 using namespace std;
 
-// Функция для разделения строки на слова
 vector<char*> splitIntoWords(char* str) {
     vector<char*> words;
     char* word = strtok(str, " ");
@@ -28,7 +27,6 @@ vector<char*> splitIntoWords(char* str) {
     return words;
 }
 
-// 1. Найти количество слов, начинающихся с буквы 'a'
 int countWordsStartingWithA(vector<char*>& words) {
     int count = 0;
     for (char* word : words) {
@@ -39,7 +37,6 @@ int countWordsStartingWithA(vector<char*>& words) {
     return count;
 }
 
-// 2. Найти длину самого короткого слова
 int findShortestWordLength(vector<char*>& words) {
     int minLength = INT_MAX;
 
@@ -53,7 +50,6 @@ int findShortestWordLength(vector<char*>& words) {
     return minLength;
 }
 
-// 3. Подсчитать количество букв 'a' в последнем слове
 int countAInLastWord(vector<char*>& words) {
     if (words.empty()) return 0;
 
@@ -69,14 +65,14 @@ int countAInLastWord(vector<char*>& words) {
     return count;
 }
 
-// 4. Заменить все буквы в верхнем регистре на строчные
+
 void convertToLowercase(char* str) {
     for (int i = 0; str[i] != '\0'; i++) {
         str[i] = tolower(str[i]);
     }
 }
 
-// 5. Найти количество слов, у которых первый и последний символы совпадают
+
 int countWordsWithMatchingEnds(vector<char*>& words) {
     int count = 0;
 
@@ -90,26 +86,25 @@ int countWordsWithMatchingEnds(vector<char*>& words) {
     return count;
 }
 
-// 6. Найти самую короткую общую подстроку между двумя словами
+
 string findShortestCommonSubstring(char* word1, char* word2) {
     int len1 = strlen(word1);
     int len2 = strlen(word2);
     string shortestSubstring = "";
     int minLength = INT_MAX;
 
-    // Перебираем все возможные подстроки первого слова
     for (int i = 0; i < len1; i++) {
         for (int j = i; j < len1; j++) {
             int subLen = j - i + 1;
             if (subLen > minLength) continue;
 
-            // Извлекаем подстроку
+   
             string substring;
             for (int k = i; k <= j; k++) {
                 substring += word1[k];
             }
 
-            // Проверяем, содержится ли подстрока во втором слове
+
             bool found = false;
             for (int m = 0; m <= len2 - subLen; m++) {
                 bool match = true;
@@ -143,43 +138,41 @@ int main() {
     cout << "===========================================" << endl;
     cout << "Введите строку из 10 слов на латинице: ";
 
-    // Ввод строки от пользователя
+
     cin.getline(input, MAX_LENGTH);
 
-    // Создаем копию строки для обработки
+
     char* workingString = new char[strlen(input) + 1];
     strcpy(workingString, input);
 
-    // Разделяем строку на слова
+
     vector<char*> words = splitIntoWords(workingString);
 
     cout << "\nРезультаты анализа строки:" << endl;
     cout << "============================" << endl;
 
-    // 1. Количество слов, начинающихся с буквы 'a'
     int wordsWithA = countWordsStartingWithA(words);
     cout << "1. Количество слов, начинающихся с буквы 'a': " << wordsWithA << endl;
 
-    // 2. Длина самого короткого слова
+
     int shortestLength = findShortestWordLength(words);
     cout << "2. Длина самого короткого слова: " << shortestLength << endl;
 
-    // 3. Количество букв 'a' в последнем слове
+
     int aInLastWord = countAInLastWord(words);
     cout << "3. Количество букв 'a' в последнем слове: " << aInLastWord << endl;
 
-    // 4. Замена заглавных букв на строчные
+
     char* lowercaseString = new char[strlen(input) + 1];
     strcpy(lowercaseString, input);
     convertToLowercase(lowercaseString);
     cout << "4. Строка в нижнем регистре: " << lowercaseString << endl;
 
-    // 5. Количество слов с совпадающими первым и последним символами
+
     int matchingEnds = countWordsWithMatchingEnds(words);
     cout << "5. Количество слов с совпадающими первым и последним символами: "
          << matchingEnds << endl;
 
-    // 6. Самая короткая общая подстрока (между первым и вторым словом)
     if (words.size() >= 2) {
         string commonSubstring = findShortestCommonSubstring(words[0], words[1]);
         if (!commonSubstring.empty()) {
@@ -189,7 +182,7 @@ int main() {
             cout << "6. Общая подстрока между первым и вторым словом не найдена" << endl;
         }
 
-        // Дополнительно: показать все слова
+
         cout << "\nСлова в строке:" << endl;
         for (int i = 0; i < words.size(); i++) {
             cout << "Слово " << (i + 1) << ": " << words[i] << endl;
@@ -198,7 +191,7 @@ int main() {
         cout << "6. Недостаточно слов для поиска общей подстроки" << endl;
     }
 
-    // Освобождение памяти
+
     delete[] workingString;
     delete[] lowercaseString;
 
